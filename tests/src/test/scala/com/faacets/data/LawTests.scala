2abf3f03f568e5228b8fbcb5bc14da157710708c
Denis Rosset <physics@denisrosset.com>
2017-03-23 15:34:52 +0800

    Removed old test for fake case class.

--- tests/src/test/scala/com/faacets/data/LawTests.scala
+++ tests/src/test/scala/com/faacets/data/LawTests.scala
@@ -1,7 +1,7 @@
 package com.faacets
 package data
 
-import org.scalacheck.{Arbitrary, Prop}
+import org.scalacheck.Arbitrary
 
 import Arbitrary.arbitrary
 
@@ -12,33 +12,6 @@ import spire.math.{Rational, SafeLong}
 import scalin.immutable.Vec
 
 import spire.algebra.Eq
-
-/*
-case class Person(name: String, age: Int /* TODO , extra: JsonObject = JsonObject.empty */) /* extends WithExtra*/
-
-object Person {
-
-  implicit val personEncodeJson: EncodeJson[Person] =
-    EncodeJson[Person] { p =>
-      jObject(p.extra.toList.foldLeft(JsonObject.fromTraversableOnce(Seq("name" -> p.name.asJson, "age" -> p.age.asJson))) { (json, jf) => json :+ jf })
-    }
-
-  implicit val personDecodeJson: DecodeJson[Person] =
-    DecodeJson[Person] { a =>
-      jdecode2L( (name: String, age: Int) => Person(name, age) )("name", "age").decode(a).map(_.copy(extra = a.focus.obj.getOrElse(JsonObject.empty) - "name" - "age"))
-    }
-
-}
-
-class PersonSuite extends FaacetsSuite {
-
-  test("Conversion to plain data and back") {
-    val john = Person("John", 45)
-    assert(john == john.asJson.jdecode[Person].fold((_,_) => sys.error("Bad result"), identity))
-  }
-
-}
-*/
 
 class LawTests extends FaacetsSuite {
 
