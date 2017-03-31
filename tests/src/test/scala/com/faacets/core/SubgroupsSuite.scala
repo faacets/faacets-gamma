f5104275cdabec7ab465d38a6b0cff416d232f88
Denis Rosset <physics@denisrosset.com>
2017-03-31 17:35:17 +0800

    Bug: output subgroups test case

--- tests/src/test/scala/com/faacets/core/SubgroupsSuite.scala
+++ tests/src/test/scala/com/faacets/core/SubgroupsSuite.scala
@@ -6,22 +6,32 @@ import net.alasc.perms.Perm
 import net.alasc.perms.default._
 
 import spire.std.map._
+import syntax.subgroups._
 
 class SubgroupsSuite extends FaacetsSuite {
 
   test("CHSH symmetry group decomposition") {
     val symGroup = Expr.CHSH.symmetryGroup
-    val subgroups = RelabelingSubgroups(symGroup)
-    subgroups.outputsSubgroup should === (Grp(rel"A0(0,1) A1(0,1) B0(0,1) B1(0,1)"))
-    subgroups.inputsOutputsSubgroup should === (Grp(
+    symGroup.outputsSubgroup should === (Grp(rel"A0(0,1) A1(0,1) B0(0,1) B1(0,1)"))
+    symGroup.inputsOutputsSubgroup should === (Grp(
       rel"A0(0,1) A1(0,1) B0(0,1) B1(0,1)",
       rel"A1(0,1) B(0,1)",
       rel"B1(0,1) A(0,1)"))
-    subgroups.partiesSubgroup should === (Grp(rel"(A,B)"))
+    symGroup.partiesSubgroup should === (Grp(rel"(A,B)"))
   }
 
   test("Outputs subgroups of Sliwa 4") {
-    RelabelingSubgroups(Expr.Sliwa4.symmetryGroup).outputPermSubgroups should === (Map((0, 1) -> Grp(Perm(0,1))))
+    Expr.Sliwa4.symmetryGroup.outputPermSubgroups should === (Map((0, 1) -> Grp(Perm(0,1))))
+  }
+
+  test("Output subgroups of zero expression") {
+    val pg = Grp(Perm(0,1))
+    Expr.zero(Scenario.CHSH).symmetryGroup.outputPermSubgroups should === (Map(
+      (0,0) -> pg,
+      (1,0) -> pg,
+      (0,1) -> pg,
+      (1,1) -> pg
+    ))
   }
 
 }
