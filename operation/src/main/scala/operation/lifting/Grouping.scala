ab7525d02eb81db868c1b513616d630a7920c218
Denis Rosset <physics@denisrosset.com>
2017-03-31 17:29:53 +0800

    Restored grouping functionality.

--- operation/src/main/scala/operation/lifting/Grouping.scala
+++ operation/src/main/scala/operation/lifting/Grouping.scala
@@ -1,30 +1,35 @@
 package com.faacets
 package operation
 package lifting
-/*
+
 import spire.algebra.Eq
-
-import net.alasc.math.Grp
-
 import core._
-import core.perm._
 import data._
+import net.alasc.finite.Grp
+import core.syntax.subgroups._
 
 case class Grouping(parties: Seq[PartyGrouping]) {
+
   override def toString = parties.mkString("[", " ", "]")
+
   def nParties: Int = parties.size
+
   def scenario: Scenario = Scenario(parties.map(_.party).toVector)
 
   def hasLiftedOutputs: Boolean = parties.exists(_.hasLiftedOutputs)
+
   def hasLiftedInputs: Boolean = parties.exists(_.hasLiftedInputs)
+
   def isLifting: Boolean = hasLiftedOutputs || hasLiftedInputs
 
   def compact = Grouping(parties.map(_.compact))
 
   def updated(p: Int, partyGrouping: PartyGrouping): Grouping = Grouping(parties.updated(p, partyGrouping))
+
 }
 
 object Grouping {
+
   def apply(scenario: Scenario, symSubgroup: Grp[Relabeling]): Grouping = {
     val trivialGrp = Grp.trivial[PartyRelabeling]
     val subgroups = symSubgroup.partyRelabelingSubgroups
@@ -32,19 +37,10 @@ object Grouping {
     Grouping(partyGroupings)
   }
 
-  def apply(expr: Expr): Grouping = expr.representation match {
-    case TRepresentation | WRepresentation =>
-      throw new IllegalArgumentException("Grouping not supported for strategies.")
-    case NPRepresentation | SPRepresentation =>
-      val symSG = expr.symmetryGroup
-      apply(expr.scenario, symSG)
-    case _ =>
-      apply(expr.to(expr.representation.permutable))
-  }
+  def apply(expr: Expr): Grouping = apply(expr.scenario, expr.symmetryGroup)
 
-  implicit val Parsable: Parsable[Grouping] = new GroupingParsable
-  implicit object Eq extends Eq[Grouping] {
-    def eqv(x: Grouping, y: Grouping) = (x == y)
-  }
+  //implicit val textable: Textable[Grouping] = new GroupingTextable
+
+  implicit val equ: Eq[Grouping] = Eq.fromUniversalEquals[Grouping]
+
 }
-*/
--- operation/src/main/scala/operation/lifting/InputGrouping.scala
+++ operation/src/main/scala/operation/lifting/InputGrouping.scala
@@ -1,48 +1,55 @@
 package com.faacets
 package operation
 package lifting
-/*
-import scala.collection.immutable
-import scala.collection.mutable
 
-import spire.syntax.action._
+import net.alasc.domains.Partition
+import net.alasc.finite.Grp
+import net.alasc.perms.Perm
+import net.alasc.perms.orbits.Points
 
-import net.alasc.math.{Grp, Perm, Domain}
-
-import core._
-import util._
-import perm.{Relabeling, PartyRelabeling}
+import scala.collection.mutable.{BitSet => MutableBitSet}
 
 /** Detection of outputs groupings.
   * 
   * For each input of each party, we find which outputs are grouped together,
   * and collect them into a `InputGrouping`.
   */
-case class InputGrouping(partition: Domain#Partition) {
+case class InputGrouping(partition: Partition) {
+
   val nOutputs = partition.size
+
   override def toString =
     if (partition.size == partition.nBlocks) partition.size.toString else (0 until partition.size).map(k => partition.blockIndex(k)).mkString("{", " ", "}")
 
   def isLiftedInput = partition.nBlocks == 1
+
   def hasLiftedOutputs = partition.blocks.exists(_.size > 1)
+
   def compact: Option[InputGrouping] = if (isLiftedInput) None else Some(InputGrouping.noLifting(partition.nBlocks))
+
 }
 
 object InputGrouping {
-  def noLifting(nOutputs: Int): InputGrouping = InputGrouping(Domain.Partition((0 until nOutputs).map(Set(_)): _*))
-  def full(nOutputs: Int): InputGrouping = InputGrouping(Domain.Partition((0 until nOutputs).toSet))
+
+  /** Singleton groupings, i.e. the input does not contain a lifting. */
+  def noLifting(nOutputs: Int): InputGrouping = InputGrouping(Partition((0 until nOutputs).map(Set(_)): _*))
+
+  /** Only one grouping containing all outputs. */
+  def full(nOutputs: Int): InputGrouping = InputGrouping(Partition((0 until nOutputs).toSet))
+
+  /** Constructs a grouping from a symmetry group of output relabelings. */
   def apply(nOutputs: Int, inputSymSubgroup: Grp[Perm]): InputGrouping = {
     val gens = inputSymSubgroup.generators
-    val remainingOutputs = mutable.BitSet.empty ++= (0 until nOutputs)
-    var groupings = Seq.empty[immutable.BitSet]
+    val remainingOutputs = MutableBitSet.empty ++= (0 until nOutputs)
+    var groupings = Seq.empty[Set[Int]]
     while (!remainingOutputs.isEmpty) {
-      import net.alasc.math.OrbitInstances._
       val output = remainingOutputs.min
-      val orbit = immutable.BitSet(output) <|+| (gens: Iterable[Perm])
+      val orbit = Points(output, gens)
       groupings = groupings :+ orbit
       remainingOutputs --= orbit
     }
-    InputGrouping(Domain.Partition(groupings: _*))
+    InputGrouping(Partition(groupings: _*))
+    // TODO: add Partition.fromPermutations(Domain(nOutputs))(gens) in alasc
   }
+
 }
-*/
