8aa2c0c5b6d5c02873196e14e4e3545c125300a6
Denis Rosset <physics@denisrosset.com>
2017-06-10 10:59:26 +0200

    RealCyclo tests with smaller sqrt/cos/sin orders.

--- laws/src/main/scala/com/faacets/laws/RealCyclos.scala
+++ laws/src/main/scala/com/faacets/laws/RealCyclos.scala
@@ -2,16 +2,23 @@ package com.faacets.laws
 
 import cyclo.RealCyclo
 import org.scalacheck.{Arbitrary, Gen}
+import spire.math.Rational
 
 object RealCyclos {
 
+  def genSmallPositiveRational: Gen[Rational] = for {
+    num <- Gen.choose(1, 4)
+    den <- Gen.choose(1, 5)
+  } yield Rational(num, den)
+
+
   def genRealCycloFromRational: Gen[RealCyclo] = Rationals.genRational.map(RealCyclo(_))
 
-  def genRealCycloFromSqrt: Gen[RealCyclo] = Rationals.genPositiveRational.map(RealCyclo.sqrt(_))
+  def genRealCycloFromSqrt: Gen[RealCyclo] = genSmallPositiveRational.map(RealCyclo.sqrt(_))
 
-  def genRealCycloFromCos: Gen[RealCyclo] = Rationals.genRational.map(RealCyclo.cosRev(_))
+  def genRealCycloFromCos: Gen[RealCyclo] = genSmallPositiveRational.map(RealCyclo.cosRev(_))
 
-  def genRealCycloFromSin: Gen[RealCyclo] = Rationals.genRational.map(RealCyclo.sinRev(_))
+  def genRealCycloFromSin: Gen[RealCyclo] = genSmallPositiveRational.map(RealCyclo.sinRev(_))
 
   def genBaseRealCyclo: Gen[RealCyclo] = Gen.oneOf(
     genRealCycloFromRational,
