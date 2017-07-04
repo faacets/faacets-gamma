22e99c07db2724a8e687586c6206944634270c64
Denis Rosset <physics@denisrosset.com>
2017-07-04 16:36:09 +0200

    Reconstruction of product bounds.

--- operation/src/main/scala/com/faacets/operation/product/BellExpressionTensor.scala
+++ operation/src/main/scala/com/faacets/operation/product/BellExpressionTensor.scala
@@ -1,15 +1,32 @@
 package com.faacets.operation.product
 
 import com.faacets.core.Expr
-import com.faacets.data.Value
+import com.faacets.data.{Scalar, Value}
 import com.faacets.operation.{BellExpression, LowerOrientation, Tensor, UpperOrientation}
+import cyclo.RealCyclo
+import spire.math.{Bounded, Interval, Point}
+import cats.syntax.traverse._
+import cats.instances.all._
 
 import scala.collection.immutable.ListMap
 
 final class BellExpressionTensor extends Tensor[BellExpression] {
 
-  def boundsProduct(components: Iterable[(Value, Value)]): Option[(Value, Value)] = {
-    None
+  def exactBoundsProduct(components: Vector[Interval[RealCyclo]]): Interval[RealCyclo] =
+    components.fold(Interval.point(RealCyclo.one))(_ * _)
+
+  def boundsProduct(components: Vector[(Value, Value)]): Option[(Value, Value)] = {
+    val exacts = components.map {
+      case (Value(Point(Scalar.Exact(l))), Value(Point(Scalar.Exact(u)))) => Some(Interval(l, u))
+      case _ => None
+    }
+    exacts.sequence.flatMap { vec =>
+      exactBoundsProduct(vec) match {
+        case Bounded(l, u, _) => Some((Value(l), Value(u)))
+        case Point(x) => Some((Value(x), Value(x)))
+        case _ => None
+      }
+    }
   }
 
   def apply(components: Map[Set[Int], BellExpression]): BellExpression = {
@@ -27,7 +44,7 @@ final class BellExpressionTensor extends Tensor[BellExpression] {
       val boundKeys = lbs intersect ubs
       val bounds: Map[String, Iterable[(Value, Value)]] =
         boundKeys.map(b => (b -> components.map { case (_, be) => (be.lower.bounds(b), be.upper.bounds(b)) })).toMap
-      val newBounds: Map[String, (Value, Value)] = bounds.flatMap { case (k, v) => boundsProduct(v).map(nv => (k -> nv)) }
+      val newBounds: Map[String, (Value, Value)] = bounds.flatMap { case (k, v) => boundsProduct(v.toVector).map(nv => (k -> nv)) }
       val newLowers = ListMap(newBounds.map { case (k, (l, _)) => (k -> l) }.toSeq: _*)
       val newUppers = ListMap(newBounds.map { case (k, (_, u)) => (k -> u) }.toSeq: _*)
       BellExpression(newExpr,
